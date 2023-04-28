94f6ce658beef9a53373483c36afd5bbb7398a5e
nasiralizade <nasir.alizade1@gmail.com>
2023-04-28 17:09:34 +0200

    std_dev

--- measurement.cpp
+++ measurement.cpp
@@ -12,6 +12,13 @@ double average_value(std::vector<double> &data) {
 }
 
 double std_dev(std::vector<double> &data) {
-    int N=data.size();
-    double
+    const size_t N = data.size();
+    double avg_val = average_value(data);
+    double dev_square = 0;
+
+    for (std::vector<double>::iterator itr = data.begin(); itr != data.end(); itr++) {
+        dev_square += pow((*itr - avg_val), 2);
+    }
+
+    return std::sqrt(dev_square * (1.0 / (N - 1)));
 }
